fixed attention mask bug

data.py
@@ -1,7 +1,7 @@
 import subprocess
 import torch
 import typing
-from torch.utils.data import DataLoader, IterableDataset
+from torch.utils.data import DataLoader, IterableDataset, Dataset
 
 from tokenizer import Tokenizer, create_tokenizer
 
@@ -60,6 +60,53 @@ class TextDataset(IterableDataset):
                 sequence.append(token)
 
 
+class TextDatasetBatched(Dataset):
+    def __init__(self, path, tokenizer, seq_len=512, fractions=(0.0, 1.0)):
+        self.fractions = fractions
+        self.seq_len = seq_len
+        self.tokenizer = tokenizer
+        self.data = self.prepare_data(path)
+
+    def prepare_data(self, path):
+        data = []
+        sequence = []
+
+        lines = line_count(path)
+        end_line = lines
+        start_line = 0
+
+        if self.fractions[0] > 0.0:
+            start_line = int(lines * self.fractions[0])
+        if self.fractions[1] < 1.0:
+            end_line = int(lines * self.fractions[1])
+
+        with open(path, "r") as f:
+            for i, line in enumerate(f):
+                # start from the right line
+                if i < start_line:
+                    continue
+
+                for token in self.tokenizer.encode(line):
+                    # seq_len + 1 since the input to the transformer is shifted by one
+                    if len(sequence) == self.seq_len + 1:
+                        data.append(sequence)
+                        sequence = []
+
+                    sequence.append(token)
+
+                # end at the right line
+                if i > end_line:
+                    break
+
+        return data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+
 def get_vocab_size():
     """Returns the size of the vocabulary"""
     encoder = create_tokenizer("./data/pg16457.txt")
@@ -75,15 +122,30 @@ def prepare_data(
     vocab_path = path.replace(".txt", ".vocab")
     tokenizer = create_tokenizer(vocab_path, bpe_path)
 
-    train_dataset = TextDataset(path, tokenizer, seq_len=seq_len, fractions=(0.0, 0.7))
-    test_dataset = TextDataset(path, tokenizer, seq_len=seq_len, fractions=(0.7, 1.0))
+    train_dataset = TextDatasetBatched(
+        path,
+        tokenizer,
+        seq_len=seq_len,
+        fractions=(0.0, 0.7),
+    )
+    test_dataset = TextDatasetBatched(
+        path,
+        tokenizer,
+        seq_len=seq_len,
+        fractions=(0.7, 1.0),
+    )
 
     train_loader = DataLoader(
-        train_dataset, batch_size=batch_size, collate_fn=lambda x: torch.tensor(x)
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        collate_fn=lambda x: torch.tensor(x),
     )
 
     val_loader = DataLoader(
-        test_dataset, batch_size=batch_size, collate_fn=lambda x: torch.tensor(x)
+        test_dataset,
+        batch_size=batch_size,
+        collate_fn=lambda x: torch.tensor(x),
     )
 
     return train_loader, val_loader, len(tokenizer.encoder)

model.py
@@ -1,7 +1,9 @@
 import math
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import pytorch_lightning as pl
+from tokenizer import create_tokenizer
 
 from utils import Config
 
@@ -22,9 +24,15 @@ class GPTBlock(nn.Module):
         self.ln_2 = nn.LayerNorm(d_embed)
         self.attn_dropout = nn.Dropout(dropout)
         self.attention = nn.MultiheadAttention(d_embed, n_heads, batch_first=True)
-        self.register_buffer("mask", torch.tril(torch.ones(block_size, block_size)))
+        self.register_buffer(
+            "mask", torch.tril(torch.ones(block_size, block_size)) == 0
+        )
+        self.mlp = lambda x: self.mlp_dict.mlp_dropout(
+            self.mlp_dict.hidden_2(self.mlp_dict.activation(self.mlp_dict.hidden_1(x)))
+        )
 
-        self.attn = lambda x: self.attn_dropout(
+    def attn(self, x):
+        return self.attn_dropout(
             self.attention(
                 x,
                 x,
@@ -32,10 +40,6 @@ class GPTBlock(nn.Module):
                 attn_mask=self.mask[: x.shape[1], : x.shape[1]],
                 need_weights=False,
             )[0]
-        )
-
-        self.mlp = lambda x: self.mlp_dict.mlp_dropout(
-            self.mlp_dict.hidden_2(self.mlp_dict.activation(self.mlp_dict.hidden_1(x)))
         )
 
     def forward(self, x) -> torch.Tensor:
@@ -53,11 +57,16 @@ class GPTTransformer(pl.LightningModule):
             config.d_embed, config.seq_len, config.n_heads, config.dropout
         )
 
-        self.pos_embeddings = nn.Embedding(config.seq_len, config.d_embed)
         self.token_embeddings = nn.Embedding(config.vocab_size, config.d_embed)
+        self.pos_embeddings = nn.Embedding(config.seq_len, config.d_embed)
         self.dropout = nn.Dropout(config.dropout)
         self.layers = nn.Sequential(*[create_block() for _ in range(config.n_layers)])
-        self.lm_head = nn.Linear(config.d_embed, config.vocab_size)
+        self.ln = nn.LayerNorm(config.d_embed)
+
+        n_params = sum(p.numel() for p in self.parameters())
+        print("number of parameters: %.2fM" % (n_params / 1e6,))
+
+        self.lm_head = nn.Linear(config.d_embed, config.vocab_size, bias=False)
 
         # initialize weights - scaling factor is taken from GPT-2 paper.
         self.apply(self._init_weights)
@@ -78,32 +87,152 @@ class GPTTransformer(pl.LightningModule):
             torch.nn.init.ones_(module.weight)
 
     def forward(self, x):
-        pos = torch.arange(0, x.size(-1), dtype=torch.long)
+        # positional embeddings
+        pos = torch.arange(0, x.shape[1], dtype=torch.long)
         pos = pos.unsqueeze(0).to(x.device)
         pos_embeddings = self.pos_embeddings(pos)
+        # token embeddings
         token_embeddings = self.token_embeddings(x)
+        # sum embeddings
         x = token_embeddings + pos_embeddings
+
         x = self.dropout(x)
         x = self.layers(x)
-        return self.lm_head(x)
+        x = self.ln(x)
+        x = self.lm_head(x)
+        return x
 
-    def training_step(self, batch, _):
+    def training_step(self, batch, idx):
         x, y = batch[:, :-1], batch[:, 1:]
         y_hat = self(x)
-        loss = nn.CrossEntropyLoss()(y_hat.permute(0, 2, 1), y)
+        loss = F.cross_entropy(y_hat.permute(0, 2, 1), y)
         self.log("train_loss", loss)
         return loss
 
-    def validation_step(self, batch, _):
+    def validation_step(self, batch, idx):
         x, y = batch[:, :-1], batch[:, 1:]
         y_hat = self(x)
-        loss = nn.CrossEntropyLoss()(y_hat.permute(0, 2, 1), y)
+        loss = F.cross_entropy(y_hat.permute(0, 2, 1), y)
         self.log("val_loss", loss)
+        if idx == 0 and self.config.should_sample:
+            print(self.generate_samples(prompt="Hello, "))
         return loss
 
     def configure_optimizers(self):
-        return torch.optim.Adam(
-            self.parameters(),
-            lr=self.config.learning_rate,
-            weight_decay=self.config.weight_decay,
+        """
+        This long function is unfortunately doing something very simple and is being very defensive:
+        We are separating out all parameters of the model into two buckets: those that will experience
+        weight decay for regularization and those that won't (biases, and layernorm/embedding weights).
+        We are then returning the PyTorch optimizer object.
+        """
+        decay = set()
+        no_decay = set()
+        for name, param in self.named_parameters():
+            if hasattr(param, "requires_grad") and not param.requires_grad:
+                continue
+            if "weight" in name and "norm" not in name and "bn" not in name:
+                decay.add(name)
+            else:
+                no_decay.add(name)
+
+        # validate that we considered every parameter
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        inter_params = decay & no_decay
+        union_params = decay | no_decay
+        assert (
+            len(inter_params) == 0
+        ), "parameters %s made it into both decay/no_decay sets!" % (str(inter_params),)
+        assert (
+            len(param_dict.keys() - union_params) == 0
+        ), "parameters %s were not separated into either decay/no_decay set!" % (
+            str(param_dict.keys() - union_params),
         )
+
+        # create the pytorch optimizer object
+        optim_groups = [
+            {
+                "params": [param_dict[pn] for pn in sorted(list(decay))],
+                "weight_decay": self.config.weight_decay,
+            },
+            {
+                "params": [param_dict[pn] for pn in sorted(list(no_decay))],
+                "weight_decay": 0.0,
+            },
+        ]
+        optimizer = torch.optim.AdamW(
+            optim_groups,
+            lr=self.config.learning_rate,
+            betas=(0.9, 0.95),
+        )
+        return optimizer
+
+    @torch.no_grad()
+    def generate(
+        self, idx, max_new_tokens, temperature=1.0, do_sample=False, top_k=None
+    ):
+        """
+        Take a conditioning sequence of indices idx (LongTensor of shape (b,t)) and complete
+        the sequence max_new_tokens times, feeding the predictions back into the model each time.
+        Most likely you'll want to make sure to be in model.eval() mode of operation for this.
+        """
+        for _ in range(max_new_tokens):
+            # if the sequence context is growing too long we must crop it at block_size
+            idx_cond = (
+                idx
+                if idx.size(1) <= self.config.seq_len
+                else idx[:, -self.config.seq_len :]
+            )
+            # forward the model te get the logits for the index in the sequence
+            logits = self(idx_cond)
+            # pluck the logits at the final step and scale by desired temperature
+            logits = logits[:, -1, :] / temperature
+            # optionally crop the logits to only the top k options
+            if top_k is not None:
+                v, _ = torch.topk(logits, top_k)
+                logits[logits < v[:, [-1]]] = -float("Inf")
+            # apply softmax to convert logits to (normalized) probabilities
+            probs = F.softmax(logits, dim=-1)
+            # either sample from the distribution or take the most likely element
+            if do_sample:
+                idx_next = torch.multinomial(probs, num_samples=1)
+            else:
+                _, idx_next = torch.topk(probs, k=1, dim=-1)
+            # append sampled index to the running sequence and continue
+            idx = torch.cat((idx, idx_next), dim=1)
+
+        return idx
+
+    @torch.no_grad()
+    def generate_samples(self, prompt="", num_samples=1, steps=20, do_sample=True):
+        """Generate text using the model."""
+        print("Generating dostoyevsky samples...")
+
+        tokenizer = create_tokenizer(
+            "./data/dostoyevsky.vocab", "./data/dostoyevsky.bpe"
+        )
+
+        if prompt == "":
+            # to create unconditional samples...
+            # manually create a tensor with only the special <|endoftext|> token
+            # similar to what openai's code does here https://github.com/openai/gpt-2/blob/master/src/generate_unconditional_samples.py
+            x = torch.tensor([[tokenizer.encoder["<|endoftext|>"]]], dtype=torch.long)
+        else:
+            x = torch.tensor([tokenizer.encode(prompt)]).to(self.device)
+
+        # we'll process all desired num_samples in a batch, so expand out the batch dim
+        x = x.expand(num_samples, -1)
+
+        # forward the model `steps` times to get samples, in a batch
+        y = self.generate(x, max_new_tokens=steps, do_sample=do_sample, top_k=40)
+
+        out = ""
+
+        for i in range(num_samples):
+            out += tokenizer.decode(y[i].cpu().squeeze().tolist())
+            out += "\n"
+            out += "-" * 80
+            out += "\n"
+
+        del tokenizer
+
+        return out

train.py
@@ -6,7 +6,7 @@ from data import prepare_data
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
 import argparse
-from tokenizer import create_tokenizer
+from model import GPTTransformer
 from model import GPTTransformer
 from utils import Config
 
@@ -24,11 +24,11 @@ def train(config: Config):
     config.vocab_size = vocab_size
 
     # set up wandb logger
-    # wandb_logger = WandbLogger(
-    #     name=config.model_name,
-    #     project="transformer-experiments",
-    #     config=vars(config),
-    # )
+    wandb_logger = WandbLogger(
+        name=config.model_name,
+        project="dostoyevsky",
+        config=vars(config),
+    )
 
     # set up the model
     model = GPTTransformer(config)
@@ -36,8 +36,9 @@ def train(config: Config):
     # set up the trainer
     trainer = pl.Trainer(
         max_epochs=config.epoch,
-        # logger=wandb_logger,
+        logger=wandb_logger,
         accelerator="gpu",
+        gradient_clip_val=1.0,
         devices=1,
     )
 
@@ -47,16 +48,23 @@ def train(config: Config):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_name", default="transformer")
-    parser.add_argument("--epoch", type=int, default=100)
-    parser.add_argument("--learning_rate", type=float, default=1e-3)
-    parser.add_argument("--batch_size", type=int, default=512)
-    parser.add_argument("--weight_decay", type=float, default=1e-5)
-    parser.add_argument("--seq_len", type=int, default=64)
-    parser.add_argument("--d_embed", type=int, default=128)
-    parser.add_argument("--n_layers", type=int, default=2)
-    parser.add_argument("--n_heads", type=int, default=2)
-    parser.add_argument("--dropout", type=float, default=0.2)
+    torch.cuda.empty_cache()
+
+    parser.add_argument("--model_name", default="gpt-mini")
+    parser.add_argument("--epoch", type=int, default=5)
+    parser.add_argument("--learning_rate", type=float, default=4e-3)
+    parser.add_argument("--batch_size", type=int, default=32)
+    # only applied to linear layers
+    parser.add_argument("--weight_decay", type=float, default=1e-2)
+    parser.add_argument("--seq_len", type=int, default=128)
+    parser.add_argument("--d_embed", type=int, default=192)
+    parser.add_argument("--n_layers", type=int, default=6)
+    parser.add_argument("--n_heads", type=int, default=6)
+    parser.add_argument("--dropout", type=float, default=0.1)
+    parser.add_argument("--gradient_norm_clip", type=float, default=1.0)
+    # should we intersperse the training with printing out samples from the model?
+    parser.add_argument("--should_sample", type=bool, default=True)
+
     args = parser.parse_args()
 
     # create the config from the args